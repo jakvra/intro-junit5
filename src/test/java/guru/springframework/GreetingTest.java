98a7014d5be228d7395f4a9fa77aea0205e3c968
Jakub Vrana <jvrana@monetplus.cz>
2018-11-01 15:33:44 +0100

    Initializing properties with junit5

---
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -0,0 +1,25 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GreetingTest {
+
+    private Greeting greeting;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("In Before Each....");
+        greeting = new Greeting();
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println(greeting.helloWorld());
+    }
+
+    @Test
+    void helloWorld1() {
+        System.out.println(greeting.helloWorld("Jakub"));
+    }
+}
